585bc5321d92c446fbd0ecd0fa65404e0c7cc48d
TC-U <2708936778@qq.com>
2026-04-19 23:27:51 +0800

    修改了按键部分

--- Hardware/Key.c
+++ Hardware/Key.c
@@ -3,12 +3,15 @@
 
 void Key_Init(void)
 {
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB,ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStrucure;
 	GPIO_InitStrucure.GPIO_Mode =GPIO_Mode_IPU;
-	GPIO_InitStrucure.GPIO_Pin =GPIO_Pin_1 | GPIO_Pin_11;
+	GPIO_InitStrucure.GPIO_Pin =GPIO_Pin_11 | GPIO_Pin_12;
 	GPIO_InitStrucure.GPIO_Speed =GPIO_Speed_50MHz;
+	GPIO_Init(GPIOA,&GPIO_InitStrucure);
+	
+	GPIO_InitStrucure.GPIO_Pin =GPIO_Pin_15;
 	GPIO_Init(GPIOB,&GPIO_InitStrucure);
 	
 }
@@ -16,19 +19,26 @@ void Key_Init(void)
 uint8_t Key_GetNum(void)
 {
 	uint8_t KeyNum=0;
-	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1)==0)
+	if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_11)==0)
 	{
 		Delay_ms(20);
-		while(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_1)==0);
+		while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_11)==0);
 		Delay_ms(20);
 		KeyNum=1;
 	}
-	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11)==0)
+	if(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_12)==0)
 	{
 		Delay_ms(20);
-		while(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_11)==0);
+		while(GPIO_ReadInputDataBit(GPIOA,GPIO_Pin_12)==0);
 		Delay_ms(20);
 		KeyNum=2;
+	}
+	if(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_15)==0)
+	{
+		Delay_ms(20);
+		while(GPIO_ReadInputDataBit(GPIOB,GPIO_Pin_15)==0);
+		Delay_ms(20);
+		KeyNum=3;
 	}
 	return KeyNum;
 }
